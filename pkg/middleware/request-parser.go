c36904e166246deeb7ac202a5e4f12a4dea9a524
Prabhu Marappan <prabhum.794@gmail.com>
2020-09-27 21:36:31 +0530

    Added RequestBody struct

--- pkg/middleware/request-parser.go
+++ pkg/middleware/request-parser.go
@@ -1,1 +1,41 @@
 package middleware
+
+import (
+	"github.com/gin-gonic/gin"
+	"io/ioutil"
+	"log"
+)
+
+type RequestBodyFormat struct {
+	ClientID string `json:"ClientID"`
+	URL string `json:"URL"`
+	Headers map[string]interface{} `json:"Headers"`
+	RequestType string `json:"RequestType"`
+	RequestBody string `json:"RequestBody"`
+}
+
+func checkRequestBody(ctx *gin.Context) error {
+	requestBody, err := ioutil.ReadAll(ctx.Request.Body)
+	if err != nil {
+		log.Println("Error with reading Request Body")
+		return err
+	}
+
+}
+
+func checkHTTPSRequest(ctx *gin.Context) error {
+
+}
+
+func RequestParser() gin.HandlerFunc {
+	return func(ctx *gin.Context) {
+		err := checkRequestBody(ctx)
+		if err != nil {
+			ctx.AbortWithError(400, err)
+		}
+		err = checkHTTPSRequest(ctx)
+		if err != nil {
+			ctx.AbortWithError(400, err)
+		}
+	}
+}
