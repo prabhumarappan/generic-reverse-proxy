d22c269058734c51107acbcf7012f30e975d2fae
Prabhu Marappan <prabhum.794@gmail.com>
2020-09-27 22:02:35 +0530

    Format error for response; add more checks for request body

--- pkg/middleware/request-parser.go
+++ pkg/middleware/request-parser.go
@@ -5,7 +5,6 @@ import (
 	"errors"
 	"github.com/gin-gonic/gin"
 	"io/ioutil"
-	"log"
 	"net/url"
 )
 
@@ -17,17 +16,28 @@ type RequestBodyFormat struct {
 	RequestBody string `json:"RequestBody"`
 }
 
+func createJSONFromError(err error) map[string]string {
+	errorMessage := err.Error()
+	response := map[string]string{
+		"error": errorMessage,
+	}
+	return response
+}
+
 func getRequestBody(ctx *gin.Context) (RequestBodyFormat, error) {
 	var rb RequestBodyFormat
 	requestBody, err := ioutil.ReadAll(ctx.Request.Body)
 	if err != nil {
-		log.Println("Error with reading Request Body")
-		return rb, err
+		return rb, errors.New("bad request body or no request body provided")
 	}
+
+	if len(requestBody) == 0 {
+		return rb, errors.New("no request body provided")
+	}
+
 	err = json.Unmarshal(requestBody, &rb)
 	if err != nil {
-		log.Println("Error with parsing request body")
-		return rb, err
+		return rb, errors.New("error with parsing the request body")
 	}
 	return rb, nil
 }
@@ -53,6 +63,9 @@ func checkRequestBody(rb RequestBodyFormat) error {
 	if (len(rb.RequestType)) == 0 {
 		return errors.New("empty request type provided")
 	}
+	if rb.RequestType != "GET" && rb.RequestType != "POST" && rb.RequestType != "PUT" && rb.RequestType != "DELETE" {
+		return errors.New("request type needs to be either GET or POST or PUT or DELETE")
+	}
 	return nil
 }
 
@@ -60,17 +73,23 @@ func RequestParser() gin.HandlerFunc {
 	return func(ctx *gin.Context) {
 		rb, err := getRequestBody(ctx)
 		if err != nil {
-			ctx.AbortWithError(400, err)
+			response := createJSONFromError(err)
+			ctx.AbortWithStatusJSON(400, response)
+			return
 		}
 
 		err = checkRequestBody(rb)
 		if err != nil {
-			ctx.AbortWithError(400, err)
+			response := createJSONFromError(err)
+			ctx.AbortWithStatusJSON(400, response)
+			return
 		}
 
 		err = checkHTTPSRequest(rb)
 		if err != nil {
-			ctx.AbortWithError(400, err)
+			response := createJSONFromError(err)
+			ctx.AbortWithStatusJSON(400, response)
+			return
 		}
 	}
 }
