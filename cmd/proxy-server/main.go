a41e8b734d6eca160f338dc86ba83be8547f6077
Prabhu Marappan <prabhum.794@gmail.com>
2020-09-28 00:03:29 +0530

    Added timeout of 5 seconds

--- cmd/proxy-server/main.go
+++ cmd/proxy-server/main.go
@@ -2,14 +2,28 @@ package main
 
 import (
 	"github.com/gin-gonic/gin"
-	"log"
 	"github.com/prabhumarappan/freshworks-hiring/pkg/api"
+	"github.com/prabhumarappan/freshworks-hiring/pkg/middleware"
+	"log"
+	"net/http"
+	"time"
 )
 
 func createServer() {
 	server := gin.Default()
-	api.StartInvocation(&server)
-	log.Fatal(server.Run(":8083"))
+	server.Use(gin.Logger())
+	server.Use(middleware.RequestParser())
+
+	api.StartInvocation(server)
+
+	srv := &http.Server{
+		Addr:              ":8083",
+		Handler:           server,
+		ReadTimeout:       5 * time.Second,
+		WriteTimeout:      5 * time.Second,
+	}
+
+	log.Fatal(srv.ListenAndServe())
 }
 
 func main()  {
