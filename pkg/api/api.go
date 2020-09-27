2092d1299da89225033de5973aa667744de80579
Prabhu Marappan <prabhum.794@gmail.com>
2020-09-28 00:03:03 +0530

    Added function to attach from request body to gin context

--- pkg/api/api.go
+++ pkg/api/api.go
@@ -6,14 +6,32 @@ import (
 	"net/http/httputil"
 )
 
+func attachRequestBodyToContext(ctx *gin.Context, requestBody middleware.RequestBodyFormat) {
+	ctx.Request.URL, _ = url.Parse(requestBody.URL)
+	if requestBody.Headers != nil {
+		for header, value := range requestBody.Headers {
+			ctx.Request.Header.Set(header, value)
+		}
+	}
+	ctx.Request.Method = requestBody.RequestType
+	bodyReader := bytes.NewReader([]byte(requestBody.RequestBody))
+	ctx.Request.Body = ioutil.NopCloser(bodyReader)
+	ctx.Request.ContentLength = bodyReader.Size()
+}
+
 func reverseProxyAPI(ctx *gin.Context) {
-	proxy := httputil.ReverseProxy{
+	rb := ctx.MustGet("RequestBody")
+	requestBody := rb.(middleware.RequestBodyFormat)
+	attachRequestBodyToContext(ctx, requestBody)
+	proxy := &httputil.ReverseProxy{
 		Director: func(req *http.Request) {
-			req.URL.Scheme =
-			req.URL.Host =
-			req.URL.Path =
-			req.Header =
-			req.Host =
+			req.URL.Scheme = ctx.Request.URL.Scheme
+			req.URL.Host = ctx.Request.URL.Host
+			req.URL.Path = ctx.Request.URL.Path
+
+			req.Header = ctx.Request.Header
+			req.Method = ctx.Request.Method
+			req.Body = ctx.Request.Body
 		},
 	}
 
