48229f873528c1182323c8a82660f4217decc388
Prabhu Marappan <prabhum.794@gmail.com>
2020-09-27 21:47:00 +0530

    Added code for request parser

--- pkg/middleware/request-parser.go
+++ pkg/middleware/request-parser.go
@@ -1,9 +1,12 @@
 package middleware
 
 import (
+	"encoding/json"
+	"errors"
 	"github.com/gin-gonic/gin"
 	"io/ioutil"
 	"log"
+	"net/url"
 )
 
 type RequestBodyFormat struct {
@@ -14,26 +17,58 @@ type RequestBodyFormat struct {
 	RequestBody string `json:"RequestBody"`
 }
 
-func checkRequestBody(ctx *gin.Context) error {
+func getRequestBody(ctx *gin.Context) (RequestBodyFormat, error) {
+	var rb RequestBodyFormat
 	requestBody, err := ioutil.ReadAll(ctx.Request.Body)
 	if err != nil {
 		log.Println("Error with reading Request Body")
-		return err
+		return rb, err
 	}
-
+	err = json.Unmarshal(requestBody, &rb)
+	if err != nil {
+		log.Println("Error with parsing request body")
+		return rb, err
+	}
+	return rb, nil
 }
 
-func checkHTTPSRequest(ctx *gin.Context) error {
+func checkHTTPSRequest(rb RequestBodyFormat) error {
+	URL, err := url.Parse(rb.URL)
+	if err != nil {
+		return err
+	}
+	if URL.Scheme != "https" {
+		return errors.New("cannot process non-https URLs")
+	}
+	return nil
+}
 
+func checkRequestBody(rb RequestBodyFormat) error {
+	if len(rb.URL) == 0 {
+		return errors.New("empty URL path provided")
+	}
+	if len(rb.ClientID) == 0 {
+		return errors.New("empty ClientId provided")
+	}
+	if (len(rb.RequestType)) == 0 {
+		return errors.New("empty request type provided")
+	}
+	return nil
 }
 
 func RequestParser() gin.HandlerFunc {
 	return func(ctx *gin.Context) {
-		err := checkRequestBody(ctx)
+		rb, err := getRequestBody(ctx)
 		if err != nil {
 			ctx.AbortWithError(400, err)
 		}
-		err = checkHTTPSRequest(ctx)
+
+		err = checkRequestBody(rb)
+		if err != nil {
+			ctx.AbortWithError(400, err)
+		}
+
+		err = checkHTTPSRequest(rb)
 		if err != nil {
 			ctx.AbortWithError(400, err)
 		}
